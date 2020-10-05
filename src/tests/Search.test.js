0ac1df5ed35385c841e034f1b657f930d2c669aa
Diana <diana.lemen@gmail.com>
2020-10-05 12:41:05 +0300

    Revert "Updates"
    This reverts commit 380f8a78323710295b7d4eb7034fd4c8d7f5f5a9.

---
+++ src/tests/Search.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import * as enzyme from 'enzyme';
+import Search from '../components/Search.js';
+
+enzyme.configure({ adapter: new Adapter() });
+
+const setImagesMock = jest.fn();
+const onClickMock = jest.fn();
+let search;
+
+beforeEach(() => {
+  search = enzyme.mount(<Search onClick= { onClickMock } setImages={ setImagesMock } />);
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('<Search /> button', () => {
+  it('shuold enabled after text is typed', () => {
+    search.find('input').simulate('change', { target: { value: 'abcdefghijk' } });
+  
+    expect(search.find('button').prop('disabled')).toBe(false);
+  });
+  
+  it('should be disabled if there is no text typed', () => {
+    search.find('input').simulate('change', { target: { value: '' } });
+  
+    expect(search.find('button').prop('disabled')).toBe(true);
+  });
+});

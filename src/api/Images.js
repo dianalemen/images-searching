c5a665a8b4f5b7c9c2c091e50fc5a535a2102b16
Diana <diana.lemen@gmail.com>
2020-10-05 11:52:26 +0300

    added spaces

--- src/api/Images.js
+++ src/api/Images.js
@@ -3,6 +3,6 @@ const key = '13417145-d0c367819415b077de5e950e3';
 
 export default function getImages(str) {
   return fetch(`${baseUrl}?key=${key}&q=${str}&image_type=photo`)
-  .then(res => res.json())
-  .catch(err => console.error(err));
+    .then(res => res.json())
+    .catch(err => console.error(err));
 }

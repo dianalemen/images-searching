0d711a2f4a024ce389329ca3ad834aa06045996b
Diana <diana.lemen@gmail.com>
2020-10-05 12:08:06 +0300

    Updates

---
+++ precache-manifest.16e7d2a9954a92e8acfaf52ffa29a44f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785bfa3439ffdd74bf0f05d267bbbb90",
+    "url": "/dianalemen/images-searching/index.html"
+  },
+  {
+    "revision": "f97b6de13a5aace19f77",
+    "url": "/dianalemen/images-searching/static/css/main.e556bfe8.chunk.css"
+  },
+  {
+    "revision": "6790192294424adb29e4",
+    "url": "/dianalemen/images-searching/static/js/2.86d21f59.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dianalemen/images-searching/static/js/2.86d21f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97b6de13a5aace19f77",
+    "url": "/dianalemen/images-searching/static/js/main.8a4f1746.chunk.js"
+  },
+  {
+    "revision": "fa33f68a9e698cf6f999",
+    "url": "/dianalemen/images-searching/static/js/runtime-main.e015aba3.js"
+  }
+]);
